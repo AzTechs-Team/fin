50831695293e7ab69c9d51733421cf8970124cb3
ashwinadiga01 <ashwinadiga0111@gmail,com>
2021-08-29 11:52:53 +0530

    still ppt :sadge:

--- src/kewl/slides.js
+++ src/kewl/slides.js
@@ -1,16 +1,10 @@
 export const slides = 
 [
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102098139545670/Screenshot_2021-06-15_at_02-26-20_kunts.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854104075716788254/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102184038629436/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102246735216650/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102303845384202/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102358152577054/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102410716774400/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102465254129674/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102495436210207/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102589405921290/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102630929137684/unknown.png",
-    "https://cdn.discordapp.com/attachments/852569805063323720/854102663947092009/unknown.png"
+    "https://cdn.discordapp.com/attachments/744882648026972180/881288911689773126/unknown.png",
+    "https://cdn.discordapp.com/attachments/744882648026972180/881288952978501673/unknown.png",
+    "https://cdn.discordapp.com/attachments/744882648026972180/881288992597868544/unknown.png",
+    "https://cdn.discordapp.com/attachments/744882648026972180/881289022486495242/unknown.png",
+    "https://cdn.discordapp.com/attachments/744882648026972180/881289249620656158/unknown.png",
+    "https://cdn.discordapp.com/attachments/744882648026972180/881289276023799817/unknown.png"
   ]
   
